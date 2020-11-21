6532e84cdf26a961fe07ca29d85e38a2912e4e3c
Natan Silnitsky <natans@wix.com>
2020-11-21 18:25:14 +0200

    functional wrappers - base

---
+++ functional-wrappers/src/main/scala/Main.scala
@@ -0,0 +1,66 @@
+package runtime.in.chunks
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.Promise
+import scala.concurrent.duration._
+import scala.util.Random
+
+import com.wixpress.dst.greyhound.core._
+import com.wixpress.dst.greyhound.core.admin.AdminClientConfig
+import com.wixpress.dst.greyhound.core.consumer.EventLoopMetric.StartingEventLoop
+import com.wixpress.dst.greyhound.core.consumer.EventLoopMetric.StoppingEventLoop
+import com.wixpress.dst.greyhound.core.consumer.domain.ConsumerRecord
+import com.wixpress.dst.greyhound.core.metrics.GreyhoundMetric
+import com.wixpress.dst.greyhound.core.producer.ProducerRecord
+import com.wixpress.dst.greyhound.future.ContextDecoder.aHeaderContextDecoder
+import com.wixpress.dst.greyhound.future.ContextEncoder.aHeaderContextEncoder
+import com.wixpress.dst.greyhound.future.ErrorHandler.anErrorHandler
+import com.wixpress.dst.greyhound.future.GreyhoundConsumer._
+import com.wixpress.dst.greyhound.future.GreyhoundProducerBuilder
+import com.wixpress.dst.greyhound.future._
+import zio.BootstrapRuntime
+import zio.Task
+import zio.UIO
+import zio.URIO
+import zio.RIO
+import zio.console
+import zio.console._
+import zio.{Promise => ZPromise}
+import zio.ZIO
+import zio.ZEnv
+
+import java.util.logging.Logger
+
+object Main extends App {
+  val kafkaPort = 9092
+  val greyhoundConfig = GreyhoundConfig(s"localhost:$kafkaPort")
+
+  implicit val ec = ExecutionContext.global
+
+  Runtime.unsafeRun {
+    for {
+      ordersRef <- zio.Ref.make[Map[String, Order]](Map.empty)
+      runtime <- ZIO.runtime[ZEnv]
+      consumersBuilder = Consumer.consumerBuilderWith(greyhoundConfig, new RecordHandler[String, String] {
+        override def handle(record: ConsumerRecord[String, String])(implicit ec: ExecutionContext): Future[Any] =
+          runtime.unsafeRunToFuture(new ZCustomHandler(ordersRef).handle(record))
+      })  
+      
+      _server = for {
+        consumer <- consumersBuilder.build
+        _ = println(">>>> consumer started")
+        producer <- GreyhoundProducerBuilder(greyhoundConfig).build
+        server = new OrdersServer(ec ,producer, InMemoryOrdersDao, ordersRef)
+      } yield server
+
+      server = Await.result(_server, 10.seconds)
+      _ = server.start()
+      _ = server.blockUntilShutdown()
+    } yield ()
+  }
+}
+
+object Runtime extends BootstrapRuntime
---
+++ functional-wrappers/src/main/scala/Orders.scala
@@ -0,0 +1,76 @@
+package runtime.in.chunks
+
+import com.example.orders.OrdersGrpc._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+import com.wixpress.dst.greyhound.core._
+
+
+import io.grpc.{Server, ServerBuilder}
+import io.grpc.protobuf.services.ProtoReflectionService
+import com.example.orders.{OrdersGrpc, CreateOrderRequest, CreateOrderReply, GetOrderRequest, GetOrderReply}
+import com.wixpress.dst.greyhound.future.GreyhoundProducer
+
+import scala.concurrent.{ExecutionContext, Future}
+import com.wixpress.dst.greyhound.core.producer.ProducerRecord
+import zio.Task
+import zio.ZIO
+
+class OrdersServer(executionContext: ExecutionContext, 
+                   producer: GreyhoundProducer,
+                   ordersDao: OrdersDao,
+                   ordersCache:  zio.Ref[Map[String, Order]]
+                   ) { self =>
+  private[this] var server: Server = null
+  private val port = 50051
+
+  def start(): Unit = {
+    server = ServerBuilder.forPort(port)
+    .addService(OrdersGrpc.bindService(new OrdersImpl, executionContext))
+    .addService(ProtoReflectionService.newInstance()).build.start
+    println("Orders Server started, listening on " + port)
+    sys.addShutdownHook {
+      System.err.println("*** shutting down gRPC server since JVM is shutting down")
+      self.stop()
+      System.err.println("*** server shut down")
+    }
+  }
+
+
+  def stop(): Unit = {
+    if (server != null) {
+      server.shutdown()
+    }
+  }
+
+  def blockUntilShutdown(): Unit = {
+    if (server != null) {
+      server.awaitTermination()
+    }
+  }
+
+  private class OrdersImpl extends OrdersGrpc.Orders {
+    implicit val _executionContext = executionContext
+    override def createOrder(req: CreateOrderRequest) = {
+      LegacyRuntime.fromTask {
+        for {
+          orderId <- ZIO.fromFuture{_ => ordersDao.createOrder(req) }
+          order <- ZIO.fromFuture{_ => ordersDao.getOrder(orderId)}
+          _ <- ordersCache.update(_.updated(orderId, order))
+          _ <- ZIO.fromFuture{_ => producer.produce(ProducerRecord("orders", orderId), Serdes.StringSerde, Serdes.StringSerde)}
+        } yield CreateOrderReply()
+      }
+    }
+
+    override  def getOrder(request: GetOrderRequest): scala.concurrent.Future[GetOrderReply] = 
+      LegacyRuntime.fromTask{ ZIO.fromFuture{_ => ordersDao.getOrder(request.orderId)}.map(Order.toReply) }
+  }
+
+}
+
+//// ZIO helpers ////
+object LegacyRuntime {
+  def fromTask[Res](body: => Task[Res]): Future[Res] = {
+    Runtime.unsafeRunToFuture(body)
+  }
+}
